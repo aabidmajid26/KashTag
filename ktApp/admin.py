73ffc44b68d2ae0203e82ec201580e070a2bdf1c
Aabid Majid <aabidmajid26@gmail.com>
2021-01-29 12:04:08 +0530

    added some tables -- except for images

--- ktApp/admin.py
+++ ktApp/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Hut
+from .models import Hut, Hotel, PonyWalla, Instructor, SkiLender,SkiShop, PhoneHotel, PhoneHut, PhoneInstructor, PhonePonyWalla, PhoneSkiLender, PhoneSkiShop
 
 admin.site.register(Hut)
+admin.site.register(Hotel)
+admin.site.register(PonyWalla)
+admin.site.register(Instructor)
+admin.site.register(SkiLender)
+admin.site.register(SkiShop)
+admin.site.register(PhoneHotel)
+admin.site.register(PhoneHut)
+admin.site.register(PhoneInstructor)
+admin.site.register(PhonePonyWalla)
+admin.site.register(PhoneSkiLender)
+admin.site.register(PhoneSkiShop)
--- ktApp/models.py
+++ ktApp/models.py
@@ -10,4 +10,57 @@ class Hut(models.Model):
 
     def __str__(self):
         return self.name + ' near ' + self.landmark
+class Hotel(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+    landmark = models.CharField(max_length=40)
 
+    def __str__(self):
+        return self.name+' near '+self.landmark
+
+class SkiShop(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+    landmark = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name+' near '+self.landmark
+
+class SkiLender(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+class Instructor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+class PonyWalla(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.name
+
+class PhonePonyWalla(models.Model):
+    fid = models.ForeignKey(PonyWalla,on_delete=models.PROTECT)
+    phone_number = models.CharField(max_length=10, unique=True)
+
+class PhoneHut(models.Model):
+    fid = models.ForeignKey(Hut,on_delete=models.PROTECT)
+    phone_number = models.CharField(max_length=10, unique=True)
+class PhoneHotel(models.Model):
+    fid = models.ForeignKey(Hotel,on_delete=models.PROTECT)
+    phone_number = models.CharField(max_length=10, unique=True)
+class PhoneSkiShop(models.Model):
+    fid = models.ForeignKey(SkiShop,on_delete=models.PROTECT)
+    phone_number = models.CharField(max_length=10, unique=True)
+class PhoneSkiLender(models.Model):
+    fid = models.ForeignKey(SkiLender,on_delete=models.PROTECT)
+    phone_number = models.CharField(max_length=10, unique=True)
+class PhoneInstructor(models.Model):
+    fid = models.ForeignKey(Instructor,on_delete=models.PROTECT)
+    phone_number = models.CharField(max_length=10, unique=True)
